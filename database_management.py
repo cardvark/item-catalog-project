fe245bb9d5fa6fc8be56a2c500bbb53aaa9e9502
James Wang <jameswang@MacBook-Pro.local>
2016-08-22 21:10:08 -0700

    Updated db management file

--- database_management.py
+++ database_management.py
@@ -97,31 +97,100 @@ def fill_categories():
 
 
 def fill_items():
-    def action_cat():
-        cat_id = get_cat_id('Action')
 
-        create_item(
+    game_tuples = [
+        (
             'Lego Star Wars: The Force Awakens',
             'It\'s a Star Wars game, but lego.  Pretty cool, man.  All the lego, all the star wars.  And full of funny shit.  Good times.',
-            cat_id,
-            1
-            )
-        create_item(
+            'Action'
+            ),
+        (
             'Mirror\'s Edge Catalyst',
             'The long awaited (maybe?) sequel to the original Mirror\'s Edge. You run and jump and parkour and stuff.  It\'s probably pretty bad for motion sickness.',
-            cat_id,
-            1
-            )
-
-    def adventure_cat():
-        cat_id = get_cat_id('Adventure')
-
-        create_item(
+            'Action'
+            ),
+        (
             'The Walking Dead',
             'Breakout hit series from Telltale Games.  Cartoonish and comicky, but only in art style.  Totally dark and fucked up. You\'re gonna make some rough decisions along the way.',
-            cat_id,
+            'Adventure'
+            ),
+        (
+            'The Wolf Among Us',
+            'Another Telltale game, based on some kind of grimm\'s fairy tales comic. It\'s pretty awesome. Very dark. Hoping to see more of this in the future.',
+            'Adventure'
+            ),
+        (
+            'Dead or Alive',
+            'Decent fighting game. Best known for its extremely buxom fighters. Has a spinoff beach volleyball game. They knew exactly what they were doing.',
+            'Fighting'
+            ),
+        (
+            'Street Fighter V',
+            'The fifth (hahaha) entry in the Street Fighter series. Okay, no idea how many entries there have been. A whole frickin lot, that\'s how many.',
+            'Fighting'
+            ),
+        (
+            'Super Mario Bros.',
+            'The original NES game. It\'s really old.',
+            'Platformer'
+            ),
+        (
+            'Super Meat Boy',
+            'You\'re a red brick of meat, and you\'re trying to save your girlfriend brick of meat, I think. Never actually played this, but it had great reviews.',
+            'Platformer'
+            ),
+        (
+            'Final Fantasy XIII',
+            'Came out for the PS3, so it\'s actually pretty old. People complained about how on-rails the early experience was. Also got a bad rap for how straight forward the combat was, early on. I liked the paradigm system though - lots of active switching was fun.',
+            'RPG'
+            ),
+        (
+            'The Walking Dead: Road to Survival',
+            'It\'s an RPG with city builder elements. Pretty significant city builder elements. Brunt of focus is around the characters and combat.',
+            'RPG'
+            ),
+        (
+            'Call of Duty',
+            'When duty calls, be quick to answer. Shoot stuff, kill folks, all that jazz.',
+            'Shooter'
+            ),
+        (
+            'Overwatch',
+            'Blizzard\'s big attempt to take on Team Fortress 2. Fairly successful so far. Good ideas. Was fun for the two matches I bothered to play.',
+            'Shooter'
+            ),
+        (
+            'Madden NFL',
+            'It\'s a game about being really mad while playing football. Or mad while inside of a foot ball. "Mad in football," so to speak.',
+            'Sports'
+            ),
+        (
+            'Rocket League',
+            'Apparently it\'s soccer, but with cars. Sounds weird. A lot of people like it, though. It\'s one of those games I figure I might like if I actually tried it out, but I don\'t really care.',
+            'Sports'
+            ),
+        (
+            'Clash of Clans',
+            'It\'s a strategy game, sort of. On mobile. Whatever.',
+            'Strategy'
+            ),
+        (
+            'Starcraft',
+            'Pretty serious series for real time strategy games. Hella popular in Korea. I enjoyed the single player campaign of Starcraft II, but never bothered w/ the multiplayer or the follow up episodes.',
+            'Strategy'
+            )
+    ]
+
+    for tup in game_tuples:
+        create_item(
+            tup[0],
+            tup[1],
+            get_cat_id(tup[2]),
             1
             )
 
-    action_cat()
-    adventure_cat()
+
+if __name__ == '__main__':
+    add_users()
+    fill_categories()
+    fill_items()
